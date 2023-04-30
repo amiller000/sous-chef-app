44629e83aa3a0edd780535f52e97699858ff8182
alexmiller226 <a.miller.22689@gmail.com>
2023-04-30 07:37:32 -0700

    Cookie cutter template commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Sous chef AI assistant',
+    author='Alex Miller',
+    license='',
+)
